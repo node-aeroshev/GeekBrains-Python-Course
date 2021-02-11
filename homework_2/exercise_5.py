0302cce7022d8d0c78f37f963c5f22687dea6ef7
nodeaeroshev <bravo2694@gmail.com>
2021-02-11 16:15:07 +0300

    Complete exercise 5

--- homework_2/exercise_5.py
+++ homework_2/exercise_5.py
@@ -1,8 +1,30 @@
 from typing import List
 
 
-rating = input("Enter a rating -> ")
+list_of_rating = []  # type: List[int]
 
-list_of_rating = [7, 5, 3, 3, 2]  # type: List[int]
+while True:
+    rating = input("Enter a rating, for stop say 'stop' -> ")
+    if rating == 'stop':
+        break
 
+    try:
+        rating = int(rating)
+    except ValueError as err:
+        print("Catch error: ", err)
+        exit(1)
 
+    count_ = list_of_rating.count(rating)
+    index_ = 0
+    try:
+        st = list_of_rating.index(rating)
+        index_ = st + count_
+    except ValueError as err:
+        index_ = 0
+        for item in list_of_rating:
+            if rating <= item:
+                index_ += 1
+
+    list_of_rating.insert(index_, rating)
+
+    print(list_of_rating)
