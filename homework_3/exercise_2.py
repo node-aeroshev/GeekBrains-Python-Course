64674f5e8caab8e190995b24bdc70b4a6ff3c1cd
nodeaeroshev <bravo2694@gmail.com>
2021-02-19 22:28:55 +0300

    Reformat exercise_2

--- homework_3/exercise_2.py
+++ homework_3/exercise_2.py
@@ -9,26 +9,26 @@ def foo(name: str,
         city: str,
         email: str,
         phone: str
-        ) -> None:
-    print(f"name: {name}\n"
-          f"surname: {surname}\n"
-          f"birthday: {birthday}\n"
-          f"city: {city}\n"
-          f"email: {email}\n"
-          f"phone: {phone}\n")
+        ) -> str:
+    return f"name: {name}\n" \
+           f"surname: {surname}\n" \
+           f"birthday: {birthday}\n" \
+           f"city: {city}\n" \
+           f"email: {email}\n" \
+           f"phone: {phone}\n"
 
 
 # Test case
-foo(phone='88005535',
-    name='John',
-    email='sample@mail.com',
-    birthday='01.01.1990',
-    city='Los Angeles',
-    surname='Lebovski')
+print(foo(phone='88005535',
+          name='John',
+          email='sample@mail.com',
+          birthday='01.01.1990',
+          city='Los Angeles',
+          surname='Lebovski'))
 
-foo(name='sample',
-    city='sample',
-    email='sample',
-    surname='mock',
-    birthday='mock',
-    phone='empty')
+print(foo(name='sample',
+          city='sample',
+          email='sample',
+          surname='mock',
+          birthday='mock',
+          phone='empty'))
