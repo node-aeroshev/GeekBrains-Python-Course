4400125b56458cb48136ded61cd72e29a9ee46c0
nodeaeroshev <bravo2694@gmail.com>
2021-03-11 18:12:35 +0300

    Complete exceptions exercises

--- homework_8/exercise_2.py
+++ homework_8/exercise_2.py
@@ -1,9 +1,25 @@
-# Спросить конкретнее
+class GlideZeroDivisionError(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
 
-class GlideZeroDivision(ZeroDivisionError):
-    def __init__(self, text: str) -> None:
-        super().__init__(text)
+    def div(self, a: float, b: float) -> float:
+        if b == 0:
+            raise self
+        return a / b
 
 
 if __name__ == '__main__':
+    handler_err = GlideZeroDivisionError("division by zero")
+    try:
+        res = handler_err.div(10, 0)
+    except GlideZeroDivisionError as err:
+        print("Handling error:", err)
+    else:
+        print("Res divided 10 / 0 =", res)
 
+    try:
+        res = handler_err.div(10, 2)
+    except GlideZeroDivisionError as err:
+        print("Handling error:", err)
+    else:
+        print("Res divided 10 / 2 =", res)
